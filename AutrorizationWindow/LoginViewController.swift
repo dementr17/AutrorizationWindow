d3558deaacdd65c6824b712363b6dcf2df5db27c
Dmitry <ecawd>
2021-12-15 23:22:43 +0300

    v3

--- AutrorizationWindow/LoginViewController.swift
+++ AutrorizationWindow/LoginViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var backgroundGradientView: UIView!
+    
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
+    
     @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var loginHint: UIButton!
     @IBOutlet weak var passwordHint: UIButton!
@@ -36,6 +38,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         var idPerform: Bool
+        
         if userNameTF.text == nameUser && passwordTF.text == passwordUser {
             idPerform = true
         } else {
@@ -43,6 +46,10 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             showAlert(title: "Error", message: "Incorrect Login and Password!")
         }
         return idPerform
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
@@ -57,25 +64,30 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         showAlert(title: "Password", message: "password")
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
+    
     
 }
 
 extension LoginViewController {
-    private func showAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
-            self.userNameTF.text = ""
-        }
-        alert.addAction(okAction)
-        present(alert, animated: true)
-    }
+    
+    override var shouldAutorotate: Bool {
+           return false
+       }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.switchBasedNextTextField(textField)
         return true
+    }
+    
+    func backgroundColor() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [#colorLiteral(red: 0, green: 0.5725490196, blue: 0.2705882353, alpha: 1).cgColor, UIColor(red: 252/255,
+                                                    green: 238/255,
+                                                    blue: 33/255,
+                                                    alpha: 1).cgColor]
+        gradientLayer.shouldRasterize = true
+        backgroundGradientView.layer.insertSublayer(gradientLayer, at:0)
     }
     
     private func switchBasedNextTextField(_ textField: UITextField) {
@@ -87,15 +99,13 @@ extension LoginViewController {
         }
     }
     
-        func backgroundColor() {
-                let gradientLayer = CAGradientLayer()
-                gradientLayer.frame = view.bounds
-        gradientLayer.colors = [#colorLiteral(red: 0, green: 0.5725490196, blue: 0.2705882353, alpha: 1).cgColor, UIColor(red: 252/255, green: 238/255, blue: 33/255, alpha: 1).cgColor]
-                gradientLayer.shouldRasterize = true
-        backgroundGradientView.layer.insertSublayer(gradientLayer, at:0)
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            self.userNameTF.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
     
-    override var shouldAutorotate: Bool {
-           return false
-       }
 }
